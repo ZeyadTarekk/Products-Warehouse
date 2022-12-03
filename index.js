a98edb0f385d4017c54f39b1c621421902d11014
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-12-03 15:50:56 +0200

    add bodyparser and cors

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import mainRouter from "./routes/routes.js";
-
+import bodyParser from "body-parser";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use((_req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,DELETE,PATCH,OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`Started on port ${PORT}`);
